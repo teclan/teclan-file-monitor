72d200da9e16f96534cca29613464f8a26b8a53a
tanbingjian <tanbingjian@maoguang.org>
2016-11-29 16:00:42 +0800

    配置文件生效

--- src/main/java/teclan/monitor/module/ConfigModule.java
+++ src/main/java/teclan/monitor/module/ConfigModule.java
@@ -1,5 +1,10 @@
 package teclan.monitor.module;
 
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.inject.AbstractModule;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
@@ -7,9 +12,16 @@ import com.typesafe.config.ConfigFactory;
 import teclan.guice.config.ConfigBinder;
 
 public class ConfigModule extends AbstractModule {
+    private final Logger LOGGER = LoggerFactory.getLogger(ConfigModule.class);
     private final String configFile;
     private final String root;
 
+    /**
+     * 加载项目目录下conf/的配置文件，如果找不到，则尝试classPath下的配置文件
+     * 
+     * @param configFile
+     * @param root
+     */
     public ConfigModule(String configFile, String root) {
         this.configFile = configFile;
         this.root = root;
@@ -18,7 +30,18 @@ public class ConfigModule extends AbstractModule {
     @Override
     protected void configure() {
 
-        Config config = ConfigFactory.load(this.configFile);
+        Config config;
+        String currentPath = System.getProperty("user.dir");
+
+        File configFile = new File(currentPath + "/conf/" + this.configFile);
+
+        if (!configFile.exists()) {
+            LOGGER.error("the config file is not found : {}",
+                    configFile.getAbsolutePath());
+            config = ConfigFactory.load(this.configFile);
+        } else {
+            config = ConfigFactory.parseFile(configFile);
+        }
 
         new ConfigBinder(binder()).bind(config, this.root);
     }
